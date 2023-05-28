c276cbabc10f82836a2ecc46c9e30fbcc6568ce3
rinesan <117470993+rinesan@users.noreply.github.com>
2023-05-28 14:03:56 +0200

    Update server.c

--- server.c
+++ server.c
@@ -60,3 +60,40 @@ int main() {
         perror("Failed to attach shared memory");
         exit(EXIT_FAILURE);
     }
+    // Initialize client count
+    client_count = 0;
+
+    // Handle client requests using different IPC mechanisms
+    handle_message_passing();
+    handle_shared_memory();
+    handle_pipes();
+
+    // Cleanup
+    shmdt(shared_memory);
+    shmctl(shmid, IPC_RMID, NULL);
+    msgctl(message_queue, IPC_RMID, NULL);
+    pthread_mutex_destroy(&mutex);
+
+    return 0;
+}
+
+void* handle_client(void* arg) {
+    struct Request* request = (struct Request*) arg;
+    struct Response response;
+
+    // Echo back the client's request
+    response.client_id = request->client_id;
+    strncpy(response.message, request->message, MAX_MESSAGE_SIZE);
+
+    // Simulate some processing time
+    sleep(1);
+
+    // Send the response back to the client
+    msgsnd(message_queue, &response, sizeof(struct Response) - sizeof(long), 0);
+ // Release the mutex
+    pthread_mutex_unlock(&mutex);
+
+    printf("Sent response to client %ld: %s\n", response.client_id, response.message);
+
+    return NULL;
+}
